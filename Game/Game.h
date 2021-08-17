85c6e64d94f48bf976a681d9fb689c8d29a02099
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2021-08-17 16:15:31 -0600

    8/17/21 Deleted Game and Actors | Added More Component types

--- Game/Game.h
+++ Game/Game.h
@@ -6,22 +6,8 @@
 class Game
 {
 public:
-	enum class GameState
-	{
-		Title,
-		StartGame,
-		StartLevel,
-		Game,
-		GameOver,
-
-	};
-
-public:
 	void Initialize();
 	void Shutdown();
-
-	bool LoadScore();
-	bool SaveScore(int score);
 
 	void Update();
 	void Draw();
@@ -29,13 +15,6 @@ public:
 	bool IsQuit() { return quit; }
 
 private:
-	void UpdateTitle(float dt);
-	void UpdateStartLevel(unsigned int enemyAmount);
-
-	void SpawnEnemies(unsigned int amount);
-
-	void OnAddPoints(const glds::Event& event);
-	void OnPlayerDeath(const glds::Event& event);
 
 public:
 	std::unique_ptr<glds::Engine> engine;
@@ -43,21 +22,4 @@ public:
 
 private:
 	bool quit{ false };
-
-	GameState state{ GameState::Title };
-	float stateTimer{ 0 };
-
-	size_t score{ 0 };
-	size_t highScore{ 0 };
-	size_t lives{ 0 };
-
-	glds::AudioChannel musicChannel;
-	std::shared_ptr<glds::Texture> particleTexture;
-	std::shared_ptr<glds::Texture> titleTexture;
-	std::shared_ptr<glds::Texture> startTexture;
-	std::shared_ptr<glds::Texture> gameOverTexture;
-	std::shared_ptr<glds::Texture> scoreTexture;
-	std::shared_ptr<glds::Texture> highScoreTexture;
-	std::shared_ptr<glds::Texture> livesTexture;
-	std::shared_ptr<glds::Font> font;
 };
